6aaeebe50c63ec9e186917e1b59fa131094f2c09
naam <reiniervanderwesten@hotmail.com>
2025-10-04 12:27:10 +0200

    Sentry toegevoegd

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
 import express from "express";
+import * as Sentry from "@sentry/node";
+import "dotenv/config";
 
 const app = express();
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({
+      tracing: true,
+    }),
+    // enable Express.js middleware tracing
+    new Sentry.Integrations.Express({
+      app,
+    }),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!,
+});
+
+// Trace incoming requests
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
