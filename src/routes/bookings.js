fbe52cfa6eb586a2e86d28310ba1c49b67ef44af
naam <reiniervanderwesten@hotmail.com>
2025-10-04 21:36:12 +0200

    bookings

--- src/routes/bookings.js
+++ src/routes/bookings.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import getBookings from "../services/bookings/getBookings.js";
+import getBookingById from "../services/bookings/getBookingById.js";
 
 const router = Router();
 
@@ -7,6 +8,21 @@ router.get("/", async (req, res, next) => {
   try {
     const bookings = await getBookings();
     res.json(bookings);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const booking = await getBookingById(id);
+
+    if (!booking) {
+      res.status(404).json({ message: `Booking with id ${id} not found` });
+    } else {
+      res.status(200).json(booking);
+    }
   } catch (error) {
     next(error);
   }
