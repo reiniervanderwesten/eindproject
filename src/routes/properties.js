10b5b3319b3956db720c7e5e4ba19863b005cb02
naam <reiniervanderwesten@hotmail.com>
2025-10-04 17:10:47 +0200

    properties compleet

--- src/routes/properties.js
+++ src/routes/properties.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import getProperties from "../services/properties/getProperties.js";
+import getPropertyById from "../services/properties/getPropertyById.js";
 
 const router = Router();
 
@@ -7,6 +8,21 @@ router.get("/", async (req, res, next) => {
   try {
     const properties = await getProperties();
     res.json(properties);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const property = await getPropertyById(id);
+
+    if (!property) {
+      res.status(404).json({ message: `property with id ${id} not found` });
+    } else {
+      res.status(200).json(property);
+    }
   } catch (error) {
     next(error);
   }
