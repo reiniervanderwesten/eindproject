284fd7fc5f080a17b7eb780519b8690ebb4bc442
naam <reiniervanderwesten@hotmail.com>
2025-10-05 19:09:56 +0200

    reviews get

--- src/routes/reviews.js
+++ src/routes/reviews.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import getReviews from "../services/reviews/getReviews.js";
+import getReviewById from "../services/reviews/getReviewById.js";
 
 
 const router = Router();
@@ -8,6 +9,21 @@ router.get("/", async (req, res, next) => {
   try {
     const reviews = await getReviews();
     res.json(reviews);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const review = await getReviewById(id);
+
+    if (!review) {
+      res.status(404).json({ message: `Review with id ${id} not found` });
+    } else {
+      res.status(200).json(review);
+    }
   } catch (error) {
     next(error);
   }
